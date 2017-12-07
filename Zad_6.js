262135137c43f78512836d26ddcffdbfde7877fe
JustynaBed <justyna.bednarko@gmail.com>
2017-12-07 23:06:07 +0100

    Ex_1_3 and 1_4

--- Zad_6.js
+++ Zad_6.js
@@ -1,4 +1,3 @@
-
 //Ex 1.1
 var sender = 'JUSTYNA';
 var sms = ' Hello Ewa, I am really tired today so I will call you maybe tomorrow morning.';
@@ -9,11 +8,10 @@ sms.trim();
 receiver = receiver.charAt(0).toUpperCase() + receiver.substring(1).toLowerCase();
 
 function message (sender, sms, receiver) {
-    return '(FROM: '+ sender + ')' + ' (TO: ' + receiver + ' ) (MESSAGE:' + sms;
+    return '(FROM: '+ sender + ')' + ' (TO: ' + receiver + ' ) (MESSAGE:' + sms + ')';
 }
 
 console.log(message(sender, sms, receiver));
-
 
 
 //Ex 1.2
@@ -28,3 +26,24 @@ function letters (firstName, lastName) {
 }
 
 console.log(letters(firstName, lastName));
+
+
+//Ex 1.3
+function RandomValue() {
+    var value = Math.random() * 10;
+    var value = value.toFixed(2);
+    return value = value;
+}
+
+console.log(RandomValue('Twoja szczęśliwa liczba to: ' + RandomValue));
+
+//Ex 1.4
+function GetDinnerPrice() {
+    var tip = 20;
+    var price = 100;
+    var priceFinal = tip + price;
+    return priceFinal;
+}
+
+console.log(GetDinnerPrice());
+
