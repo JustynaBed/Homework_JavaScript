396ea8437770b8b841faaec0e9401bfd261290c4
JustynaBed <justyna.bednarko@gmail.com>
2017-12-08 22:20:24 +0100

    Ready to review @pukie

--- Zad_6.js
+++ Zad_6.js
@@ -1,10 +1,10 @@
-//Ex 1.1
+//Ex 6.1
 var sender = 'JUSTYNA';
 var sms = ' Hello Ewa, I am really tired today so I will call you maybe tomorrow morning.';
 var receiver = 'EWA';
 
 sender = sender.charAt(0).toUpperCase() + sender.substring(1).toLowerCase();
-sms.trim();
+sms = sms.trim();
 receiver = receiver.charAt(0).toUpperCase() + receiver.substring(1).toLowerCase();
 
 function message (sender, sms, receiver) {
@@ -14,7 +14,7 @@ function message (sender, sms, receiver) {
 console.log(message(sender, sms, receiver));
 
 
-//Ex 1.2
+//Ex 6.2
 var firstName = 'justyna';
 var lastName = 'bednarko';
 
@@ -28,32 +28,32 @@ function letters (firstName, lastName) {
 console.log(letters(firstName, lastName));
 
 
-//Ex 1.3
-function RandomValue() {
-    var value = Math.random() * 10;
-    var value = value.toFixed(2);
-    return value;
-}
-
-console.log('Twoja szczęśliwa liczba to: ' + RandomValue());
-
-//Ex 1.3-SECOND OPTION
-function RandomValueTwo() {
+//Ex 6.3
+function randomValue() {
     var value = Math.random() * 10;
     var value = value.toFixed(2);
     return value = value;
 }
 
-console.log('Twoja szczęśliwa liczba to: ' + RandomValueTwo());
+console.log('Twoja szczęśliwa liczba to: ' + randomValue());
+
+//Ex 6.3-SECOND OPTION
+function randomValueTwo() {
+    var value = Math.random() * 10;
+    var value = value.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
+    return value = value;
+}
+
+console.log('Twoja szczęśliwa liczba to: ' + randomValueTwo());
 
 
-//Ex 1.4
-function GetDinnerPrice() {
+//Ex 6.4
+function getDinnerPrice() {
     var tip = 20;
     var price = 100;
     var priceFinal = tip + price;
     return priceFinal;
 }
 
-console.log(GetDinnerPrice());
+console.log(getDinnerPrice());
 
